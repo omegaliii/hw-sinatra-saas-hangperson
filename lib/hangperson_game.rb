a3a5c20830571d680d0d22055a452e7b962a53ae
Jianhui Li <omegali047@gmail.com>
2019-02-06 23:29:58 -0800

    Ready for Heroku!

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,9 +7,60 @@ class HangpersonGame
 
   # def initialize()
   # end
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+  attr_accessor :word_with_guesses
+  attr_accessor :check_win_or_lose
+
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+    @word_with_guesses = word.gsub(/[[:alpha:]]/, "-")
+    @check_win_or_lose = :play
+    @wrong_count = 0
+  end
+
+  def guess(guessChar)
+    if guessChar == '' || guessChar == nil || guessChar !~ /[[:alpha:]]/
+      raise ArgumentError
+    end
+
+    guessChar = guessChar.downcase 
+
+    if @word.include? guessChar
+      if !@guesses.include? guessChar
+        @guesses = @guesses + guessChar
+
+        indices = (0 ... @word.length).find_all { |i| @word[i,1] == guessChar }
+        indices.each do |i|
+          @word_with_guesses[i] = guessChar
+        end
+
+        if(@word_with_guesses == @word) 
+          @check_win_or_lose = :win
+        end
+
+        true
+      else
+        false
+      end
+    else
+      if !@wrong_guesses.include? guessChar
+        @wrong_guesses = @wrong_guesses + guessChar
+        @wrong_count = @wrong_count + 1
+        
+        if(@wrong_count >= 7)
+          @check_win_or_lose = :lose
+        end
+    
+        true
+      else
+        false
+      end
+    end
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
